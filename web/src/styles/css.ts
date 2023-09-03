6e4f48f66120577eb2ae10f2891252765778e6de
Berk Cambaz <berkcambaz12321@gmail.com>
2023-09-03 20:43:32 +0300

    Update css.ts

--- web/src/styles/css.ts
+++ web/src/styles/css.ts
@@ -8,5 +8,5 @@ export const emoji: React.CSSProperties = {
 }
 
 export const textShadow: React.CSSProperties = {
-  textShadow: "-2px -2px 0 rgba(0, 0, 0, 0.5), 2px -2px 0 rgba(0, 0, 0, 0.5), -2px 2px 0 rgba(0, 0, 0, 0.5), 2px 2px 0 rgba(0, 0, 0, 0.5)"
+  textShadow: "-1px -1px 0 rgba(0, 0, 0, 1), 1px -1px 0 rgba(0, 0, 0, 1), -1px 1px 0 rgba(0, 0, 0, 1), 1px 1px 0 rgba(0, 0, 0, 1)"
 }
