f86a9fb9365f15c9fb15120e61da5c55439a827e
Berk Cambaz <berkcambaz12321@gmail.com>
2023-09-03 21:02:59 +0300

    Basic monster items component

--- web/src/components/_game/MonsterItems.tsx
+++ web/src/components/_game/MonsterItems.tsx
@@ -1,6 +1,35 @@
+import { Button, Flex } from "@mantine/core"
+import Emoji from "../Emoji"
+
 function MonsterItems() {
   return (
-    <>items</>
+    <>
+      <Flex direction="column" align="center">
+        <Button variant="default" h="auto" p="md">
+          <Emoji emoji="⭐" style={{ width: 32, height: 32 }} />
+        </Button>
+      </Flex>
+
+      <Flex direction="row" justify="center" gap="xl">
+        <Button variant="default" h="auto" p="md" mr="xl">
+          <Emoji emoji="⭐" style={{ width: 32, height: 32 }} />
+        </Button>
+
+        <Button variant="default" h="auto" p="md" ml="xl">
+          <Emoji emoji="⭐" style={{ width: 32, height: 32 }} />
+        </Button>
+      </Flex>
+
+      <Flex direction="row" justify="center" gap="md">
+        <Button variant="default" h="auto" p="md">
+          <Emoji emoji="⭐" style={{ width: 32, height: 32 }} />
+        </Button>
+
+        <Button variant="default" h="auto" p="md">
+          <Emoji emoji="⭐" style={{ width: 32, height: 32 }} />
+        </Button>
+      </Flex>
+    </>
   )
 }
 
