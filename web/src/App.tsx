c66c598defc56a0a02a71ef5c4c30a4bda5c4666
Berk Cambaz <berkcambaz12321@gmail.com>
2023-09-03 23:53:35 +0300

    Make back button work on top bar

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -26,7 +26,10 @@ function App() {
       <Flex pos="fixed" maw={theme.breakpoints.sm} w="100%" h={48} top={0} style={{ zIndex: 1000, background: theme.colors.dark[7] }}>
         <Card withBorder m="md" w="100%" h="100%">
           <Flex h="100%" direction="row" align="center" justify="space-between">
-            <ActionIcon>
+            <ActionIcon
+              style={route !== "home" ? {} : { visibility: "hidden" }}
+              onClick={() => navigate("/home")}
+            >
               <IconArrowLeft />
             </ActionIcon>
 
