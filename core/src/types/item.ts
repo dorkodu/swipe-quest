54e546aa18868fa9136d4466225d52297c74f2b1
Berk Cambaz <berkcambaz12321@gmail.com>
2023-09-04 18:39:41 +0300

    Update item.ts

--- core/src/types/item.ts
+++ core/src/types/item.ts
@@ -22,24 +22,21 @@ export interface IItem {
   spd?: number;
 }
 
-export type _WeaponId = keyof typeof weapon;
-export const weaponData: Record<`Common ${_WeaponId}` | `Rare ${_WeaponId}` | `Legendary ${_WeaponId}`, IItem> = {} as any;
+type IItemId<T extends string> = `Common ${T}` | `Rare ${T}` | `Legendary ${T}`
+
+export const weaponData: Record<IItemId<keyof typeof weapon>, IItem> = {} as any;
 export type WeaponId = keyof typeof weaponData;
 
-export type _ArmorId = keyof typeof armor;
-export const armorData: Record<`Common ${_ArmorId}` | `Rare ${_ArmorId}` | `Legendary ${_ArmorId}`, IItem> = {} as any;
+export const armorData: Record<IItemId<keyof typeof armor>, IItem> = {} as any;
 export type ArmorId = keyof typeof armorData;
 
-export type _RuneId = keyof typeof rune;
-export const runeData: Record<`Common ${_RuneId}` | `Rare ${_RuneId}` | `Legendary ${_RuneId}`, IItem> = {} as any;
+export const runeData: Record<IItemId<keyof typeof rune>, IItem> = {} as any;
 export type RuneId = keyof typeof runeData;
 
-export type _RingId = keyof typeof ring;
-export const ringData: Record<`Common ${_RingId}` | `Rare ${_RingId}` | `Legendary ${_RingId}`, IItem> = {} as any;
+export const ringData: Record<IItemId<keyof typeof ring>, IItem> = {} as any;
 export type RingId = keyof typeof ringData;
 
-export type _AmuletId = keyof typeof amulet;
-export const amuletData: Record<`Common ${_AmuletId}` | `Rare ${_AmuletId}` | `Legendary ${_AmuletId}`, IItem> = {} as any;
+export const amuletData: Record<IItemId<keyof typeof amulet>, IItem> = {} as any;
 export type AmuletId = keyof typeof amuletData;
 
 function setData(data: any, source: any) {
