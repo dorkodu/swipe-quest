0b3bf7eab8eb5cfff87d62e25018c011aca06314
Berk Cambaz <berkcambaz12321@gmail.com>
2023-09-05 14:48:59 +0300

    Update MonsterStats.tsx

--- web/src/components/_game/MonsterStats.tsx
+++ web/src/components/_game/MonsterStats.tsx
@@ -11,12 +11,14 @@ interface Props {
 }
 
 function MonsterStats({ level, hp, dmg, spd }: Props) {
+  const power = game.util.getMonsterPower(hp, dmg, spd);
+
   return (
     <Flex direction="column" gap="md" w="100%">
       <Flex gap="md" wrap="wrap">
         <Flex gap="xs">
           <Emoji emoji="⚡" style={{ width: 24, height: 24 }} />
-          <Title order={5}>{`Power: ${game.util.getMonsterPower(hp, dmg, spd)}`}</Title>
+          <Title order={5}>{`Power: ${util.formatNumber(power)}`}</Title>
         </Flex>
         <Flex gap="xs">
           <Emoji emoji="⭐" style={{ width: 24, height: 24 }} />
