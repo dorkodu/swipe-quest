9bb5bef853b712aa3269aad839c3bb955b22ce54
Berk Cambaz <berkcambaz12321@gmail.com>
2023-09-07 13:05:57 +0300

    Move monster data to constants.json and dynamically generate

--- core/src/types/monster.ts
+++ core/src/types/monster.ts
@@ -1,4 +1,5 @@
 import monster from "../data/monster.json";
+import { constants } from "./constants";
 import { ItemId } from "./item";
 
 export interface IMonster {
@@ -21,4 +22,24 @@ export interface IMonsterData {
 }
 
 export type MonsterId = keyof typeof monster;
-export const monsterData: Record<MonsterId, IMonsterData> = monster;
+export const monsterData: Record<MonsterId, IMonsterData> = {} as any;
+
+// TODO: Fix types
+function setData(
+  data: Record<any, IMonsterData>,
+  source: Record<any, IMonsterData>
+) {
+  for (const [key, value] of Object.entries(source)) {
+    data[key] = {
+      ...value,
+      baseHp: value._id * constants.MONSTER_BASEHP * constants.MONSTER_BASEHP_MODIFIER,
+      baseDmg: value._id * constants.MONSTER_BASEDMG * constants.MONSTER_BASEDMG_MODIFIER,
+      baseSpd: value._id * constants.MONSTER_BASESPD * constants.MONSTER_BASESPD_MODIFIER,
+    };
+  }
+}
+
+setData(
+  monsterData,
+  monster as any,
+)
