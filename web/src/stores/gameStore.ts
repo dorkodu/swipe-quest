259977079cc2137bb4695a64b3344408f0ffb0ae
Berk Cambaz <berkcambaz12321@gmail.com>
2023-09-09 22:21:05 +0300

    Migrate zustand game store to not lose user progression

--- web/src/stores/gameStore.ts
+++ web/src/stores/gameStore.ts
@@ -27,7 +27,43 @@ export const useGameStore = create(
         },
       }),
       {
-        name: "game"
+        name: "game",
+        version: 1,
+        migrate(persistedState, version) {
+          const state = persistedState as any;
+
+          switch (version) {
+            case 0:
+              delete state.data.campaign.stage;
+              state.data.dailyMissionDate = Date.now();
+              state.data.dailyMissions = {
+                progressCampaign: { count: 0 },
+                progressTower: { count: 0 },
+                killMonster: { count: 0 },
+                unlockMonster: { count: 0 },
+                unlockItem: { count: 0 },
+                evolveMonster: { count: 0 },
+                upgradeItem: { count: 0 },
+                swipeGameEvents: { count: 0 },
+                completeAllDailyMissions: { count: 0 },
+              };
+              state.data.rebirth = {
+                points: 0,
+
+                multipliers: {
+                  xp: 1,
+                  gold: 1,
+                  diamond: 1,
+                  food: 1,
+                  item: 1,
+                  monster: 1,
+                }
+              }
+              break;
+          }
+
+          return state;
+        },
       }
     )
   )
