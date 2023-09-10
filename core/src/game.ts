9a07b80933109368a7b996541efa3dd9c555e4c3
Berk Cambaz <berkcambaz12321@gmail.com>
2023-09-10 19:36:14 +0300

    Fix daily missions signal bug

--- core/src/game.ts
+++ core/src/game.ts
@@ -2,6 +2,9 @@ import { gameplay } from "./gameplay";
 import { createGameData } from "./gamedata";
 import { util } from "./lib/util";
 
+// Import daily missions here so the signals are initialized
+import "./types/daily_missions";
+
 export const game = {
   play: gameplay,
   util: util,
